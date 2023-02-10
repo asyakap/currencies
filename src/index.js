c1bdda259541d070e342583f1830aa6d7222e890
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-02-10 10:29:59 -0800

    populate currencies list from the call

--- src/index.js
+++ src/index.js
@@ -4,53 +4,49 @@ import './css/styles.css';
 import Currencies from './js/ExchangeRate.js';
 
 // Business Logic
-function getUSExchangeRates() {
-  Currencies.getUSExchangeRates()
-    .then(function (response) {
-      if (response.main) {
-        printElements(response);
-      } else {
-        printError(response);
-      }
-    });
-}
 
 // UI Logic
-function populateSelects(reference){
+function populateSelects(reference) {
+  console.log(reference.conversion_rates);
   let currencies = Object.keys(reference.conversion_rates);
-  for(let i = 0; i < currencies.length; i++){
-    $("#exchange-from").append("<option>" + currencies[i] + "</option>");
-    $("#exchange-to").append("<option>" + currencies[i] + "</option>");
+  console.log(currencies);
+  let exchangeFrom = document.querySelector("select#exchange-from");
+  let exchangeTo = document.querySelector("select#exchange-to");
+  for (let i = 0; i < currencies.length; i++) {
+    let opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = currencies[i];
+    exchangeFrom.appendChild(opt);
+    opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = currencies[i];
+    exchangeTo.appendChild(opt);
+    
   }
 }
 
-async function callForCurrencies(){
+async function callForCurrencies() {
   const response = await Currencies.getUSExchangeRates();
+  console.log(response);
   populateSelects(response);
 }
 
-function printElements(response) {
-
+async function callForAnyRate(convertFrom, convertTo, amount) {
+  const response = await Currencies.getAnyExchangeRate(convertFrom);
+  let conversion = response.conversion_rates[convertTo] * amount;
+  return conversion;
 }
-
-function printError(response) {
-  document.querySelector('#showResponse').innerText = `There was an error accessing the currencies exchange data: ADD ERROR.`;
-}
-
-
-
-
 
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  let convertFrom = $('#exchange-from').val();
-  let convertTo = $('#exchange-to').val();
-  let exchangeAmount = $('#amount').val();
-  callForAnyRate(convertFrom, convertTo, exchangeAmount);
+  let convertFrom = document.getElementById("#exchange-from").val();
+  let convertTo = document.getElementById("#exchange-to").val();
+  let exchangeAmount = document.getElementById("#amount").val();
+  document.getElementById("showResponse").innerText = callForAnyRate(convertFrom, convertTo, exchangeAmount);
 }
 
 window.addEventListener("load", function () {
-  callForCurrencies("exchange-from");
-  document.querySelector('form#form-conversion').addEventListener("submit", handleFormSubmission);
+  callForCurrencies();
+  document.querySelector("form#form-conversion").addEventListener("submit", handleFormSubmission);
 });
--- src/js/ExchangeRate.js
+++ src/js/ExchangeRate.js
@@ -1,32 +1,32 @@
 export default class Currencies {
   static getUSExchangeRates() {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(function(response) {
+      .then(function (response) {
         if (!response.ok) {
-        throw new Error(response.statusText);
-      } else {
-        return response.json();
-      }
-  })
-  .catch(function(error) {
-    return error;
-  });
-}
-
-static async getAnyExchangeRate(convertFrom){
-  try{
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${convertFrom}`);
-    if(response.status === 404){
-      throw Error("Please enter an existing currency!");
-    }
-    if(!response.ok){
-      throw Error(response.status);
-    }
-    return await response.json();
-  } catch(error) {
-    $('#error').innerHTML = error;
-    $('#output-area').show();
-    $('#error').show();
+          throw new Error(response.statusText);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        return error;
+      });
   }
-}
+
+  static async getAnyExchangeRate(convertFrom) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${convertFrom}`);
+      if (response.status === 404) {
+        throw Error("Please enter an existing currency!");
+      }
+      if (!response.ok) {
+        throw Error(response.status);
+      }
+      return await response.json();
+    } catch (error) {
+      document.getElementById('#showResponse').innerHTML = error;
+      document.getElemenetById('#output-area').show();
+      document.getElemenetById('#showResponse').show();
+    }
+  }
 }
