2164177b33f4d93cdd98beb84d14a9b6286c5efe
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-02-10 09:57:55 -0800

    add functions to index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './css/styles.css';
 import Currencies from './js/ExchangeRate.js';
 
 // Business Logic
-function getExchangeRates() {
-  Currencies.getExchangeRates()
+function getUSExchangeRates() {
+  Currencies.getUSExchangeRates()
     .then(function (response) {
       if (response.main) {
         printElements(response);
@@ -16,6 +16,19 @@ function getExchangeRates() {
 }
 
 // UI Logic
+function populateSelects(reference){
+  let currencies = Object.keys(reference.conversion_rates);
+  for(let i = 0; i < currencies.length; i++){
+    $("#exchange-from").append("<option>" + currencies[i] + "</option>");
+    $("#exchange-to").append("<option>" + currencies[i] + "</option>");
+  }
+}
+
+async function callForCurrencies(){
+  const response = await Currencies.getUSExchangeRates();
+  populateSelects(response);
+}
+
 function printElements(response) {
 
 }
@@ -24,3 +37,20 @@ function printError(response) {
   document.querySelector('#showResponse').innerText = `There was an error accessing the currencies exchange data: ADD ERROR.`;
 }
 
+
+
+
+
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let convertFrom = $('#exchange-from').val();
+  let convertTo = $('#exchange-to').val();
+  let exchangeAmount = $('#amount').val();
+  callForAnyRate(convertFrom, convertTo, exchangeAmount);
+}
+
+window.addEventListener("load", function () {
+  callForCurrencies("exchange-from");
+  document.querySelector('form#form-conversion').addEventListener("submit", handleFormSubmission);
+});
